f8e2b2ce20af91037f615adfed01fb597c15346d
jayrnoel <8006157+jayrnoel@users.noreply.github.com>
2022-09-08 20:15:23 +0800

    cleanup

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const email = document.querySelector(".email");
 const errorMsg = document.querySelector(".error__message");
-const submit = document.querySelector(".submit__email");
+const form = document.querySelector(".subscribe");
+const submitBtn = document.querySelector(".submit__email");
 const emailRegex =
   /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i;
 
 function validate(email) {
+  if (email === "") {
+    return "Whoops! It looks like you forgot to add your email";
+  }
   if (emailRegex.test(email)) {
     return true;
   } else {
-    return false;
+    return "Please provide a valid email address.";
   }
 }
-
 email.addEventListener("keyup", () => {
-  //   console.log(email.value);
-  if (!validate(email.value)) {
+  if (validate(email.value) !== true) {
+    let error = validate(email.value);
     email.classList.add("error");
     errorMsg.style.display = "block";
-    console.log("error");
+    errorMsg.innerText = error;
   } else {
     email.classList.remove("error");
     errorMsg.style.display = "none";
   }
-  console.log(validate(email.value));
+});
+
+submitBtn.addEventListener("click", () => {
+  if (validate(email.value) !== true) {
+    let error = validate(email.value);
+    email.classList.add("error");
+    errorMsg.style.display = "block";
+    errorMsg.innerText = error;
+  }
 });
